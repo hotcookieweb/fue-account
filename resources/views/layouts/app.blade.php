cca7b5daa8d633cce3d222c7d783fe2e2b6978d1
Jesse Horne <j.horne2796@gmail.com>
2018-12-22 09:57:17 -0700

    completed issue #13

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Hot Cookie Mobile Access') }}</title>
+    <title>{{ config('app.name', 'Hot Cookie Kitchen') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'HotCookie Kitchen') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,15 +33,17 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-						<li class="nav-item">
-							<a class="nav-link" href="/">{{ __('Home') }}</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="/orders">{{ __('Orders') }}</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="/board">{{ __('Board') }}</a>
-						</li>
+          						<li class="nav-item">
+          							<a class="nav-link" href="/">{{ __('Home') }}</a>
+          						</li>
+                      @if(Auth::user())
+            						<li class="nav-item">
+            							<a class="nav-link" href="/orders">{{ __('Orders') }}</a>
+            						</li>
+            						<li class="nav-item">
+            							<a class="nav-link" href="/board">{{ __('Board') }}</a>
+            						</li>
+                      @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
