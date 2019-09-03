2a8765f84208078aafbbf6c6c6a17d637f0df119
Jesse Horne <j.horne2796@gmail.com>
2019-09-03 12:30:11 -0500

    testing

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -52,8 +52,8 @@ class BoardController extends Controller
         }
 
         $new_data["delivery_date"] = $order['meta_data'][0]['value'] ? $order['meta_data'][0]['value'] : "None";
-        return $new_data["delivery_time"];
         $new_data["delivery_time"] = $order['meta_data'][1]['value'] ? $order['meta_data'][1]['value'] : "None";
+        return $new_data["delivery_time"];
         if (explode(" ", $new_data["delivery_time"])[0] !== "Between") {
           $new_data["delivery_time"] = "None";
         }
