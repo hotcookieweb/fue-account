3e941ac3a045deda7b0e0ce1dee4471cc832ed5a
tony <tony@hotcookiebakery.com>
2019-11-13 12:54:34 -0800

    changes to order display

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,140 +1,106 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">#{{ $order['number'] }} {{ $order['billing']['first_name'] }} {{ $order['billing']['last_name'] }} ({{ $order['status'] }})</div>
-
-                <div class="card-body">
-                  <p>
-                    @if($order)
-                      <i>Created </i><b>{{ date("F d, Y", strtotime($order['date_created'])) }}</b><br>
-
-                      <br>
-
-                      <?php
-                      foreach ($order['meta_data'] as $md) {
-                        if ($md["key"] == "ready_date") {
-                          ?>
-                          <b>Ready Date</b>: {{ $md["value"] }}<br>
-                          <?php
-                        }
-                        if ($md["key"] == "ready_time") {
-                          ?>
-                          <b>Ready Time</b>: {{ $md["value"] }}<br>
-                          <?php
-                        }
-                      }
-                      ?>
-
-                      <br>
-
-                      <div class="row">
-                        <div class="col-lg-6">
-                          <h3>Billing</h3>
-
-                          <p>
-                            {{ $order['billing']['first_name'] }} {{ $order['billing']['last_name'] }}<br>
-                            Phone # {{ $order['billing']['phone'] }}<br>
-                            Email - <i>{{ $order['billing']['email'] }}</i><br>
-                            {{ $order['billing']['address_1'] }}<br>
-                            @if($order['billing']['address_2'])
-                              {{ $order['billing']['address_2'] }}<br>
-                            @endif
-                            {{ $order['billing']['city'] }}, {{ $order['billing']['state'] }} {{ $order['billing']['postcode'] }}<br>
-                            {{ $order['billing']['country'] }}
-                          </p>
-                        </div>
-
-                        <div class="col-lg-6">
-                          <h3>Shipping</h3>
-
-                          <p>
-                            {{ $order['shipping']['first_name'] }} {{ $order['shipping']['last_name'] }}<br>
-                            {{ $order['shipping']['address_1'] }}<br>
-                            @if($order['shipping']['address_2'])
-                              {{ $order['shipping']['address_2'] }}<br>
-                            @endif
-                            {{ $order['shipping']['city'] }}, {{ $order['shipping']['state'] }} {{ $order['shipping']['postcode'] }}<br>
-                            {{ $order['shipping']['country'] }}
-                          </p>
-                        </div>
-                      </div>
-
-                      <br>
-
-                      <h2>Items</h2>
-                      <table class="table">
-                        <thead>
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Cost</th>
-                            <th scope="col">Qty</th>
-                            <th scope="col">Total</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          @foreach($order['line_items'] as $i=>$item)
-                            <tr>
-                              <th scope="row">{{ $i+1 }}</th>
-                              <?php
-                                $meta = "";
-                                foreach ($item["meta_data"] as $md) {
-                                  if ($md["key"] == "pa_cookie-flavor") {
-                                    $key = "Cookie Flavor";
-                                  } else if ($md["key"] == "pa_chocolate-type") {
-                                    $key = "Chocolate Type";
-                                  } else if ($md["key"] == "pa_delivery-shipping-options") {
-                                    $key = "Delivery & Shipping Options";
-                                  } else if ($md["key"] == "pa_fabrication") {
-                                    $key = "Fabrication";
-                                  } else if ($md["key"] == "pa_features") {
-                                    $key = "Features";
-                                  } else if ($md["key"] == "pa_ingredients") {
-                                    $key = "Ingredients";
-                                  } else if ($md["key"] == "pa_size") {
-                                    $key = "Size";
-                                  } else if ($md["key"] == "pa_toppers") {
-                                    $key = "Sexy Toppers";
-                                  } else {
-                                    $key = $md["key"];
-                                  }
-                                  $meta = $meta . "<b>" . $key . "</b> - " . $md["value"] . "<br>";
-                                }
-                              ?>
-                              <td>
-                                {{ $item["name"] }}<br>
-                                {!! $meta !!}
-                              </td>
-                              <td>${{ $item['price'] }}</td>
-                              <td>{{ $item['quantity'] }}</td>
-                              <td>${{ $item['total'] }}</td>
-                            </tr>
-                          @endforeach
-                        </tbody>
-                      </table>
-
-                      <h5>Shipping</h5>
-                      @foreach($order['shipping_lines'] as $line)
-                        <ul>
-                          <li>{{ $line['method_title'] }} - ${{ $line['total']}}</li>
-                        </ul>
-                      @endforeach
-
-                      <br>
-
-                      @if($order['customer_note'])
-                        <h3>Customer Note</h3>
-                        <p>{{ $order['customer_note'] }}</p>
-                      @endif
-                    @endif
-                  </p>
-                </div>
-            </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-header">
+            <h4 class="card-title">Order #: {{ $order['number'] }}</h4>
+          </div>
+          <div class="card-body">
+            Date created: {{ date("F d, Y", strtotime($order['date_created'])) }}<br>
+            Order status: {{ $order['status'] }}<br>
+            {{ $order['shipping_lines']['0']['method_title'] }}: {{ $order['shipping_lines']['0']['total']}}<br>
+            @foreach ($order['meta_data'] as $md)
+              @if ($md["key"] == "ready_date")
+                Ready date: {{ $md["value"] }}<br>
+              @endif
+              @if ($md["key"] == "ready_time")
+                Ready time: {{ $md["value"] }}<br>
+              @endif
+            @endforeach
+          </div>
         </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-header">
+            <h4 class="card-title">Billing Address</h4>
+          </div>
+          <div class="card-body">
+            {{ $order['billing']['first_name'] }} {{ $order['billing']['last_name'] }}<br>
+            {{ $order['billing']['address_1'] }}<br>
+            @if($order['billing']['address_2'])
+              {{ $order['billing']['address_2'] }}<br>
+            @endif
+            {{ $order['billing']['city'] }}, {{ $order['billing']['state'] }} {{ $order['billing']['postcode'] }} {{ $order['billing']['country'] }}<br>
+            Phone # {{ $order['billing']['phone'] }}<br>
+            Email - <i>{{ $order['billing']['email'] }}</i><br>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-header">
+            <h4 class="card-title">Shipping Address</h4>
+          </div>
+          <div class="card-body">
+            {{ $order['shipping']['first_name'] }} {{ $order['shipping']['last_name'] }}<br>
+            {{ $order['shipping']['address_1'] }}<br>
+            @if($order['shipping']['address_2'])
+              {{ $order['shipping']['address_2'] }}<br>
+            @endif
+            {{ $order['shipping']['city'] }}, {{ $order['shipping']['state'] }} {{ $order['shipping']['postcode'] }} {{ $order['shipping']['country'] }}<br>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="row justify-content-left">
+      <div class="card-header">
+        <h4 class="card-title">Items</h4>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Cost</th>
+            <th scope="col">Qty</th>
+            <th scope="col">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($order['line_items'] as $i=>$item)
+            <tr>
+              <th scope="row">{{ $i+1 }}</th>
+              <td>
+                {{ $item["name"] }}<br>
+                @foreach ($item["meta_data"] as $md)
+                  @php
+                  $key = str_replace("pa_", "", $md["key"]); // meta data passed as slugs, not names
+                  @endphp
+                  {{ $key }}: {{ $md["value"] }}<br>
+                @endforeach
+              </td>
+              <td>${{ $item['price'] }}</td>
+              <td>{{ $item['quantity'] }}</td>
+              <td>${{ $item['total'] }}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    <div class="row justify-content-left">
+      @if($order['customer_note'])
+        <div class="card-header">
+          <h4 class="card-title">Customer Note:</h4>
+        </div>
+        <div class="card-body">
+          {{ $order['customer_note'] }}
+        </div>
+      @endif
+    </div>
+  </div>
 </div>
 @endsection
